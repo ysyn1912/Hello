67840a1565ec3ee5adfdb4febc4cedbef83aecc7
zht <15339839929@189.cn>
2016-06-23 17:41:58 +0800

    2016.6.23 17：41添加注释

--- src/com/gtf/test/Calc.java
+++ src/com/gtf/test/Calc.java
@@ -2,18 +2,42 @@ package com.gtf.test;
 
 public class Calc {
 
+	/**
+	 * 加法计算
+	 * @param firstNum 第一个数字
+	 * @param SecondNum 第二个数字
+	 * @return double类型的数值
+	 */
 	static public double add(double firstNum, double SecondNum) {
 		return firstNum + SecondNum;
 	}
-	
+
+	/**
+	 * 减法计算
+	 * @param firstNum 第一个数字
+	 * @param SecondNum 第二个数字
+	 * @return double类型的数值
+	 */
 	static public double sub(double firstNum, double SecondNum) {
 		return firstNum - SecondNum;
 	}
-	
+
+	/**
+	 * 乘法计算
+	 * @param firstNum 第一个数字
+	 * @param SecondNum 第二个数字
+	 * @return double类型的数值
+	 */
 	static public double mul(double firstNum, double SecondNum) {
 		return firstNum * SecondNum;
 	}
-	
+
+	/**
+	 * 除法计算
+	 * @param firstNum 第一个数字
+	 * @param SecondNum 第二个数字
+	 * @return double类型的数值
+	 */
 	static public double div(double firstNum, double SecondNum) {
 		return firstNum / SecondNum;
 	}
