a9aa6f09f691e40ce03900a6b3f11f30db2da884
zht <15339839929@189.cn>
2016-06-23 17:29:44 +0800

    2016.6.23 项目提交

---
+++ src/com/gtf/test/Calc.java
@@ -0,0 +1,21 @@
+package com.gtf.test;
+
+public class Calc {
+
+	static public double add(double firstNum, double SecondNum) {
+		return firstNum + SecondNum;
+	}
+	
+	static public double sub(double firstNum, double SecondNum) {
+		return firstNum - SecondNum;
+	}
+	
+	static public double mul(double firstNum, double SecondNum) {
+		return firstNum * SecondNum;
+	}
+	
+	static public double div(double firstNum, double SecondNum) {
+		return firstNum / SecondNum;
+	}
+	
+}
---
+++ src/com/gtf/test/Main.java
@@ -0,0 +1,17 @@
+package com.gtf.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Calc.add(6, 2));
+		
+		System.out.println(Calc.sub(6, 2));
+		
+		System.out.println(Calc.mul(6, 2));
+		
+		System.out.println(Calc.div(6, 2));
+		
+	}
+	
+}
